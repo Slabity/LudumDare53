41ef83089a903dc8e4080c4423691d3bcaf44944
Christopher Chin <ctchin13@gmail.com>
2023-04-29 10:24:37 -0400

    Update Cooldowns for Godot 4

--- tests/unit/test_cooldowns.gd
+++ tests/unit/test_cooldowns.gd
@@ -1,9 +1,10 @@
 extends GutTest
 
-# This shouldn't go below the physics tick time. About 17ms for 60fps.
+# Seconds. This shouldn't go below the physics tick time. About 17ms for 60fps.
 const ADD_WAIT_TIME = 0.4
-# Seems GUT is flakey with its yield_time so this wait buffer is VERY large.
-const WAIT_BUFFER = 0.2
+# GUT used to be very flakey with its yield times. Perhaps it is working in
+# Godot 4?
+const WAIT_BUFFER = 0
 
 var _cooldowns
 var _callback_called
@@ -33,34 +34,34 @@ func test_add():
 	assert_true(_cooldowns.exists("0"))
 	assert_false(_cooldowns.exists("1"))
 
-	yield(yield_for(ADD_WAIT_TIME / 2.0), YIELD)
+	await wait_seconds(ADD_WAIT_TIME / 2.0)
 	assert_true(_cooldowns.exists("0"))
 
-	yield(yield_for(ADD_WAIT_TIME / 2.0 + WAIT_BUFFER), YIELD)
+	await wait_seconds(ADD_WAIT_TIME / 2.0 + WAIT_BUFFER)
 	assert_false(_cooldowns.exists("0"))
 
 
 func test_remaining():
 	_cooldowns.add("0", 2)
-	assert_almost_eq(_cooldowns.remaining("0"), 2, 0.02)
+	assert_almost_eq(_cooldowns.remaining("0"), 2.0, 0.02)
 
-	yield(yield_for(1), YIELD)
-	assert_almost_eq(_cooldowns.remaining("0"), 1, 0.2)
-	yield(yield_for(1.2), YIELD)
+	await wait_seconds(1)
+	assert_almost_eq(_cooldowns.remaining("0"), 1.0, 0.2)
+	await wait_seconds(1.2)
 	assert_eq(_cooldowns.remaining("0"), 0.0)
 
 
 func test_callback_no_arg():
 	_cooldowns.add("0", ADD_WAIT_TIME, self, "_on_cooldown_no_arg")
 	assert_false(_callback_called)
-	yield(yield_for(ADD_WAIT_TIME + WAIT_BUFFER), YIELD)
+	await wait_seconds(ADD_WAIT_TIME + WAIT_BUFFER)
 	assert_true(_callback_called)
 
 
 func test_callback_arg():
 	_cooldowns.add("0", ADD_WAIT_TIME, self, "_on_cooldown_arg", 123)
 	assert_false(_callback_called)
-	yield(yield_for(ADD_WAIT_TIME + WAIT_BUFFER), YIELD)
+	await wait_seconds(ADD_WAIT_TIME + WAIT_BUFFER)
 	assert_true(_callback_called)
 
 
@@ -86,16 +87,10 @@ func test_cooldowns_spawner():
 	_cooldowns.add("2", ADD_WAIT_TIME)
 	assert_eq(_cooldowns.get_children().size(), 3)
 
-	yield(yield_for(ADD_WAIT_TIME + WAIT_BUFFER), YIELD)
+	await wait_seconds(ADD_WAIT_TIME + WAIT_BUFFER)
 	assert_eq(_cooldowns.get_children().size(), 3)
 	_cooldowns.add("3", ADD_WAIT_TIME)
 	_cooldowns.add("4", ADD_WAIT_TIME)
 	_cooldowns.add("5", ADD_WAIT_TIME)
 	_cooldowns.add("6", ADD_WAIT_TIME)
 	assert_eq(_cooldowns.get_children().size(), 4)
-
-
-func test_frames_time():
-	_cooldowns.add("0", _cooldowns.frames_time(3))
-	yield(yield_for(ADD_WAIT_TIME), YIELD)
-	assert_false(_cooldowns.exists("0"))
