788253d5cfc8d613d713ff6c372ed363fe95f5a1
redragon6466 <willfenner656@gmail.com>
2023-04-30 16:00:35 -0400

    Removed gut added wall detection
    removed the gut addon, added wall detection to enemies

--- src/scenes/EnemyScript.gd
+++ src/scenes/EnemyScript.gd
@@ -5,22 +5,22 @@ var is_stealing = false
 var stealing_speed_mod = 2
 var dir_to_run_in_x  = 0
 
-const SPEED = 50.0
+const SPEED = 100.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var collision_tracker_array_left : Array = []
 var collision_tracker_array_right : Array = []
 
+var wall_collision_tracker_array_left : Array = []
+var wall_collision_tracker_array_right : Array = []
+
 func _on_area_2d_body_entered(body):
 	if(body.is_in_group("Player")):
 		is_stealing = true;
-		print(body.position)
-		print(self.position)
 		if ((body.position - self.position).normalized().x > 0):
 			dir_to_run_in_x = -1
 		else:
 			dir_to_run_in_x = 1
 			
-		print(dir_to_run_in_x)
 		var node = get_child(2)
 		if(node != null):
 			node.visible = true
@@ -35,11 +35,16 @@ func _physics_process(delta):
 		move_and_slide()
 		return
 		
-		
-	if(collision_tracker_array_right.size() == 0):
+	if(collision_tracker_array_right.size() == 0 && dir):
 		dir = false
 	
-	if(collision_tracker_array_left.size() == 0):
+	if(collision_tracker_array_left.size() == 0 && not dir):
+		dir = true
+		
+	if(wall_collision_tracker_array_right.size() != 0 && dir):
+		dir = false
+	
+	if(wall_collision_tracker_array_left.size() != 0 && not dir):
 		dir = true
 		
 	if(dir):
@@ -79,3 +84,32 @@ func _on_floor_dector_right_body_entered(body):
 			return
 			
 	collision_tracker_array_right.append(body)	
+
+
+func _on_wall_dector_right_body_entered(body):
+	for i in range(wall_collision_tracker_array_right.size()):
+		if (wall_collision_tracker_array_right[i] == body):
+			return
+			
+	wall_collision_tracker_array_right.append(body)	
+
+
+
+func _on_wall_dector_right_body_exited(body):
+	for i in range(wall_collision_tracker_array_right.size()):
+		if (wall_collision_tracker_array_right[i] == body):
+			wall_collision_tracker_array_right.remove_at(i)
+			return
+
+func _on_wall_dector_left_body_entered(body):
+	for i in range(wall_collision_tracker_array_left.size()):
+		if (wall_collision_tracker_array_left[i] == body):
+			return
+			
+	wall_collision_tracker_array_left.append(body)	
+
+func _on_wall_dector_left_body_exited(body):
+	for i in range(wall_collision_tracker_array_left.size()):
+		if (wall_collision_tracker_array_left[i] == body):
+			wall_collision_tracker_array_left.remove_at(i)
+			return
