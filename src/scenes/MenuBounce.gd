43c62dbfc57fe0b44e04ba42a1eac9ccb3676cab
Fritz Wallace <john.fritz.wallace@gmail.com>
2023-04-30 13:30:11 -0400

    Added 2 images, and script to rotate them on main menu

---
+++ src/scenes/MenuBounce.gd
@@ -0,0 +1,17 @@
+extends Sprite2D
+var rotateValue
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	rotateValue = .01
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	rotate(rotateValue)
+	if(rotation > -.3):
+		rotateValue = rotateValue*-1
+	if(rotation < .3):
+		rotateValue = rotateValue*-1
+	pass
