55b3a2ee86dabd02a0591154426b58b50733301f
redragon6466 <willfenner656@gmail.com>
2023-04-30 20:59:23 -0400

    Quest system iteration one
    Added a quest system composed of a quest hub and 4 quest destinations at each of the location. pressing f at the quest hub brings up the options and leaving the hub sets your destination

--- src/scenes/main_scene.gd
+++ src/scenes/main_scene.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_main_menu_play_game():
-	get_tree().change_scene_to_file("res://src/scenes/levels/map001.tscn")
+	get_tree().change_scene_to_file("res://src/scenes/levels/full_world.tscn")
 	print("TODO: Play game")
 
 
