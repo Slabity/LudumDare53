8c46980567d04a48680d015944cb693f5a4ffece
redragon6466 <willfenner656@gmail.com>
2023-05-01 13:25:27 -0400

    Added consequences for stealing
    Added consequences for letting your stuff get stolen. as well as fixing the timer so it stared and stops based on quest system. Added theme to timer and quest finished text.

--- src/scenes/levels/map001.gd
+++ src/scenes/levels/map001.gd
@@ -5,22 +5,26 @@ extends Node2D
 var started = false
 var start_time
 var completion_time
+var time_delta
+var time_delta_unix
 
+func _ready():
+	timer_label.text = ""
 
 func _process(_delta):
 	if started:
 		var time = Time.get_unix_time_from_system()
-		var time_delta = Time.get_time_string_from_unix_time(time - start_time)
+		time_delta_unix = time - start_time
+		time_delta = Time.get_time_string_from_unix_time(time_delta_unix)
 		timer_label.text = time_delta + ":%02d" % int(fmod(time, 1) * 100)
 
-
-func _on_start_area_body_entered(_body: Node2D):
-	if !started:
-		started = true
-		start_time = Time.get_unix_time_from_system()
-
-
-func _on_win_area_body_entered(_body: Node2D):
+func _on_quest_system_quest_completed(time_completed_string, best_time):
 	if started:
 		started = false
+		timer_label.text = ""
 		completion_time = Time.get_unix_time_from_system()
+
+
+func _on_quest_system_quest_started():
+	started = true
+	start_time = Time.get_unix_time_from_system()
