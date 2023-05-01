84d3d077333bf03c7e391484626afce38bbd87e4
redragon6466 <willfenner656@gmail.com>
2023-05-01 14:21:17 -0400

    Stealing is bad (#33)
    * Added consequences for stealing
    
    Added consequences for letting your stuff get stolen. as well as fixing the timer so it stared and stops based on quest system. Added theme to timer and quest finished text.
    
    * Remove no longer needed start area/win area and clean up names
    
    Removed the start area and win area for the timer, as timer is set by quest system now. Also named the pit cube, and hub background nodes.
    
    ------

--- assets/enemy/EnemyScript.gd
+++ assets/enemy/EnemyScript.gd
@@ -2,8 +2,10 @@ extends CharacterBody2D
 
 var dir = true
 var is_stealing = false
+var can_steal = true
 var stealing_speed_mod = 2
 var dir_to_run_in_x = 0
+var last_player_collision
 
 const SPEED = 100.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -13,18 +15,32 @@ var collision_tracker_array_right: Array = []
 var wall_collision_tracker_array_left: Array = []
 var wall_collision_tracker_array_right: Array = []
 
+signal asking_for_a_friend_do_you_have_treasure(thief: Node)
+signal alright_you_got_me_have_your_stuff_back
+
 
 func _on_area_2d_body_entered(body):
-	if body.is_in_group("Player"):
-		is_stealing = true
-		if (body.position - self.position).normalized().x > 0:
-			dir_to_run_in_x = -1
-		else:
-			dir_to_run_in_x = 1
+	if body.is_in_group("Player") && can_steal:
+		last_player_collision = body.position
+		asking_for_a_friend_do_you_have_treasure.emit(self)
 
+
+func steal(has_treasure):
+	#because I want it muahahahhahaha
+	if has_treasure:
 		var node = get_child(2)
 		if node != null:
 			node.visible = true
+
+		node = get_child(3)
+		if node != null:
+			node.visible = true
+
+		is_stealing = true
+		if (last_player_collision - self.position).normalized().x > 0:
+			dir_to_run_in_x = -1
+		else:
+			dir_to_run_in_x = 1
 
 
 func _physics_process(delta):
@@ -114,3 +130,26 @@ func _on_wall_dector_left_body_exited(body):
 		if wall_collision_tracker_array_left[i] == body:
 			wall_collision_tracker_array_left.remove_at(i)
 			return
+
+
+func _on_stomp_detector_body_entered(body):
+	if is_stealing:
+		print("you got me")
+		is_stealing = false
+
+		var node = get_child(2)
+		if node != null:
+			node.visible = false
+
+		node = get_child(3)
+		if node != null:
+			node.visible = false
+
+		alright_you_got_me_have_your_stuff_back.emit()
+		steal_cooldown()
+
+
+func steal_cooldown():
+	can_steal = false
+	await get_tree().create_timer(10).timeout
+	can_steal = true
