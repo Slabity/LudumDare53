4eac88ba82df3c22d14d7858006713d7ce75bbe6
Christopher Chin <ctchin13@gmail.com>
2023-05-01 14:49:36 -0400

    Make sound sliders work

--- src/settings.gd
+++ src/settings.gd
@@ -4,7 +4,7 @@ const SETTINGS_PATH = "user://settings"
 
 var settings = {}
 
-@export var default_settings = {"vol_master": 40, "vol_music": 100, "vol_sfx": 100}
+@export var default_settings = {"vol_master": 0.4, "vol_music": 1.0, "vol_sfx": 1.0}
 
 
 func _ready():
@@ -21,6 +21,17 @@ func load_settings():
 		var file = FileAccess.open(SETTINGS_PATH, FileAccess.READ)
 		settings = file.get_var()
 		file.close()
+
+	# Apply sound settings to the bus.
+	AudioServer.set_bus_volume_db(
+		AudioServer.get_bus_index("Master"), linear_to_db(settings["vol_master"])
+	)
+	AudioServer.set_bus_volume_db(
+		AudioServer.get_bus_index("Music"), linear_to_db(settings["vol_music"])
+	)
+	AudioServer.set_bus_volume_db(
+		AudioServer.get_bus_index("Effects"), linear_to_db(settings["vol_sfx"])
+	)
 
 
 func save_settings():
