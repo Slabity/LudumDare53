21b8ff8f22c59b154124c5b06c1d3f20f235afbc
redragon6466 <willfenner656@gmail.com>
2023-04-30 20:59:23 -0400

    Quest system iteration one
    Added a quest system composed of a quest hub and 4 quest destinations at each of the location. pressing f at the quest hub brings up the options and leaving the hub sets your destination

--- src/scenes/player/responsive_camera/responsive_camera.gd
+++ src/scenes/player/responsive_camera/responsive_camera.gd
@@ -14,3 +14,12 @@ func _ready():
 func _process(delta):
 	if target_node:
 		position = lerp(position, target_node.global_position, lerp_speed)
+
+
+func _on_quest_system_quest_completed(time_completed):
+	var node = get_child(1)
+	if node != null:
+		node.visible = true
+		node.text = "You did it! Final Time: " + time_completed
+		await get_tree().create_timer(3).timeout
+		node.visible = false
