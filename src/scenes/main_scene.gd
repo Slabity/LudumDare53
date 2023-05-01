170b875ba91daec372cbd0d7bfae05805e6320e3
Tyler Slabinski <tslabinski@slabity.net>
2023-04-30 22:41:10 -0400

    Fix menus and add theme

--- src/scenes/main_scene.gd
+++ src/scenes/main_scene.gd
@@ -1,9 +1,12 @@
 extends Control
 
 
+func _ready():
+	$CenterContainer/MainMenu.open_main()
+
+
 func _on_main_menu_play_game():
 	get_tree().change_scene_to_file("res://src/scenes/levels/map001.tscn")
-	print("TODO: Play game")
 
 
 func _on_main_menu_exit_menu():
---
+++ src/scenes/menus/settings_menu.gd
@@ -0,0 +1,61 @@
+extends Control
+
+signal exit_menu
+
+@onready var current_menu = $MainSettings
+@onready var settings_node = get_node("/root/Settings")
+@onready var settings = settings_node.settings
+
+
+func open_main_settings():
+	switch_to_menu($MainSettings)
+	$MainSettings/AudioButton.grab_focus()
+
+
+func open_audio_settings():
+	$AudioSettings/Master/Slider.set_value(settings["vol_master"])
+	$AudioSettings/Music/Slider.set_value(settings["vol_music"])
+	$AudioSettings/SFX/Slider.set_value(settings["vol_sfx"])
+	$AudioSettings/Master/Slider.grab_focus()
+	switch_to_menu($AudioSettings)
+
+
+func switch_to_menu(menu: Node):
+	current_menu.hide()
+	current_menu = menu
+	current_menu.show()
+
+
+# MainSettings signals
+
+
+func _on_audio_button_pressed():
+	open_audio_settings()
+
+
+func _on_video_button_pressed():
+	print("TODO: VideoButton pressed()")
+
+
+func _on_main_back_button_pressed():
+	settings_node.save_settings()
+	exit_menu.emit()
+
+
+# AudioSettings signals
+
+
+func _on_master_slider_value_changed(value: float):
+	settings["vol_master"] = value
+
+
+func _on_music_slider_value_changed(value: float):
+	settings["vol_music"] = value
+
+
+func _on_sfx_slider_value_changed(value: float):
+	settings["vol_sfx"] = value
+
+
+func _on_audio_back_button_pressed():
+	open_main_settings()
