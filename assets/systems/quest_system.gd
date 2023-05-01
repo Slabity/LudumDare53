9e29e2f2b183a72274c167430ef23a7bdafa0a6a
redragon6466 <willfenner656@gmail.com>
2023-05-01 13:25:27 -0400

    Added consequences for stealing
    Added consequences for letting your stuff get stolen. as well as fixing the timer so it stared and stops based on quest system. Added theme to timer and quest finished text.

--- assets/systems/quest_system.gd
+++ assets/systems/quest_system.gd
@@ -1,11 +1,18 @@
 extends Node2D
 
 const Objectives = {
-	ObjectiveTower = "Tower",
-	ObjectiveDungeon = "Dungeon",
-	ObjectivePit = "Pit",
-	ObjectiveOutskirts = "Outskirts",
-	NoObjective = ""
+	ObjectiveTower = 4,
+	ObjectiveDungeon = 1,
+	ObjectivePit =2,
+	ObjectiveOutskirts = 3,
+	NoObjective = 0
+}
+
+var BestTime = {
+	Tower = 999999999999999999,
+	Dungeon = 999999999999999999,
+	Pit =999999999999999999,
+	Outskirts = 999999999999999999,
 }
 
 @onready var sound_quest_complete = $QuestComplete
@@ -15,30 +22,50 @@ var player_is_in_quest_hub = false
 var is_menu_visible = false
 var current_objective = Objectives.NoObjective
 var were_questing = false
-var target = Vector2(0, 0)
-@onready var timer_node = get_node("../Camera2D/Timer")
-signal quest_completed(time_completed_string: String)
-signal target_set(target: Node)
+var target
+var current_thief
 
+@onready var map_node = get_node("..")
+@onready var enemy_holder_node = get_node("../EnemyHolder")
+signal quest_completed(time_completed_string: String, best_time_string: String)
+signal target_set(target: Node)
+signal quest_started()
+
+func _ready():
+	if(enemy_holder_node):
+		for i in enemy_holder_node.get_child_count():
+			var node = enemy_holder_node.get_child(i)
+			node.connect("asking_for_a_friend_do_you_have_treasure", ProcessTheft)
+			node.connect("alright_you_got_me_have_your_stuff_back", ProcessCounterTheft)
 
 func _physics_process(delta):
 	if player_is_in_quest_hub:
 		if Input.is_action_just_released("Interact"):
 			var node = get_child(5)
-			if node != null:
-				node.visible = true
+			if (set_node_visibility(5, true, false)):
 				is_menu_visible = true
+			
+			set_node_visibility(0, false, false)
+	
+	
 
-			node = get_child(0)
-			if node != null:
-				node.visible = false
+func ProcessTheft(thief):
+	if(current_objective != Objectives.NoObjective && not current_thief):
+		#........ I do have treasure why do you ask?
+		thief.steal(true)
+		target_set.emit(thief)
+		current_thief = thief
+		set_node_visibility(current_objective, false, false)
 
+func ProcessCounterTheft():
+	current_thief = null
+	if(set_node_visibility(current_objective, true, true)):
+			target_set.emit(target)
+		
 
 func _on_quest_hub_area_body_entered(body):
 	player_is_in_quest_hub = true
-	var node = get_child(0)
-	if node != null:
-		node.visible = true
+	set_node_visibility(0, true, false)
 
 
 func _on_quest_hub_area_body_exited(body):
@@ -47,42 +74,26 @@ func _on_quest_hub_area_body_exited(body):
 	if node != null:
 		node.visible = false
 	if is_menu_visible:
-		node = get_child(5)
-		if node != null:
-			node.visible = false
+		if(set_node_visibility(5, false, false)):
 			is_menu_visible = false
 	if current_objective != Objectives.NoObjective:
 		were_questing = true
+		quest_started.emit()
+		if(set_node_visibility(current_objective, true, true)):
+			target_set.emit(target)
+			sound_quest_update.play()
 
-		match current_objective:
-			Objectives.ObjectiveTower:
-				node = get_child(4)
-				if node != null:
-					node.visible = true
-					target = node.position
-					target_set.emit(target)
-					sound_quest_update.play()
-			Objectives.ObjectiveDungeon:
-				node = get_child(1)
-				if node != null:
-					node.visible = true
-					target = node.position
-					target_set.emit(target)
-					sound_quest_update.play()
-			Objectives.ObjectivePit:
-				node = get_child(2)
-				if node != null:
-					node.visible = true
-					target = node.position
-					target_set.emit(target)
-					sound_quest_update.play()
-			Objectives.ObjectiveOutskirts:
-				node = get_child(3)
-				if node != null:
-					node.visible = true
-					target = node.position
-					target_set.emit(target)
-					sound_quest_update.play()
+
+func set_node_visibility(child_to_toggle: int, state_to_set: bool,set_current_target: bool):
+	var node = get_child(child_to_toggle)
+	if node != null:
+		node.visible = state_to_set
+		if(set_current_target):
+			target = node
+		return true
+	
+	return false
+	
 
 
 func _on_outskirts_button_pressed():
@@ -100,35 +111,52 @@ func _on_pit_button_pressed():
 func _on_tower_button_pressed():
 	current_objective = Objectives.ObjectiveTower
 
+func finish_quest(child_to_hide):
+	if (not current_thief):
+		current_objective = Objectives.NoObjective
+		var node = get_child(child_to_hide)
+		if node != null:
+			node.visible = false
+		
+		var time = Time.get_unix_time_from_system()
+		var delta = map_node.time_delta_unix
+		var run_best_time = 0
+		match(child_to_hide):
+			Objectives.ObjectiveDungeon:
+				if(BestTime.Dungeon > delta):
+					BestTime.Dungeon = delta
+				run_best_time =BestTime.Dungeon
+			Objectives.ObjectiveOutskirts:
+				if(BestTime.Outskirts > delta):
+					BestTime.Outskirts = delta
+				run_best_time =BestTime.Outskirts
+			Objectives.ObjectivePit:
+				if(BestTime.Pit > delta):
+					BestTime.Pit = delta
+				run_best_time =BestTime.Pit
+			Objectives.ObjectiveTower:
+				if(BestTime.Tower > delta):
+					BestTime.Tower = delta
+				run_best_time =BestTime.Tower
+		
+		quest_completed.emit(map_node.time_delta + ":%02d" % int(fmod(time, 1) * 100), run_best_time)
 
 func _on_dungeon_quest_objective_area_body_entered(body):
 	if body.is_in_group("Player") && current_objective == Objectives.ObjectiveDungeon:
-		var node = get_child(1)
-		if node != null:
-			node.visible = false
+		finish_quest(1)
 
-		quest_completed.emit(timer_node.text)
-		sound_quest_complete.play()
 
 
 func _on_pit_quest_objective_area_body_entered(body):
 	if body.is_in_group("Player") && current_objective == Objectives.ObjectivePit:
-		var node = get_child(2)
-		if node != null:
-			node.visible = false
+		finish_quest(2)
 
-		quest_completed.emit(timer_node.text)
-		sound_quest_complete.play()
 
 
 func _on_outskirts_quest_objective_area_body_entered(body):
 	if body.is_in_group("Player") && current_objective == Objectives.ObjectiveOutskirts:
-		var node = get_child(3)
-		if node != null:
-			node.visible = false
-
-		quest_completed.emit(timer_node.text)
-		sound_quest_complete.play()
+		finish_quest(3)
+		
 
 
 func _on_tower_quest_objective_area_body_entered(body):
@@ -139,3 +167,5 @@ func _on_tower_quest_objective_area_body_entered(body):
 
 		quest_completed.emit(timer_node.text)
 		sound_quest_complete.play()
+		finish_quest(4)
+
